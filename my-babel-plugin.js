cca6d5b567e36bd075ec758a93953a1b22b957ba
duwon1229 <44364759+duwon1229@users.noreply.github.com>
2021-09-30 23:58:11 +0900

    babel first setting(preset)

--- my-babel-plugin.js
+++ my-babel-plugin.js
@@ -4,6 +4,7 @@ module.exports = function myBabelPlugin() {
       VariableDeclaration(path) {
         console.log("VariableDeclaration() kind:", path.node.kind); // const
 
+        // const => var 변환
         if (path.node.kind === "const") {
           path.node.kind = "var";
         }
