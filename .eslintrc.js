aa30fced4223064460ade2837ab4557a5ea943a7
duwon1229 <44364759+duwon1229@users.noreply.github.com>
2021-10-18 00:58:51 +0900

    prettier, 자동화

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,13 @@ module.exports = {
         "browser": true,
         "es2021": true
     },
-    "extends": "eslint:recommended",
+    "extends": ["eslint:recommended","eslint-config-prettier"],
     "parserOptions": {
         "ecmaVersion": 13,
         "sourceType": "module"
     },
+    plugins: ["prettier"],
     "rules": {
+        "prettier/prettier": "error"
     }
 };
