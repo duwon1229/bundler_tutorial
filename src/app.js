20f7ad6528ab44c0507e3fbcd90941ecbe503411
duwon1229 <44364759+duwon1229@users.noreply.github.com>
2021-10-29 00:30:29 +0900

    핫 로딩

--- src/app.js
+++ src/app.js
@@ -1,8 +1,28 @@
-import axios from "axios";
+import form from "./form";
+import result from "./result";
+
+let resultEl;
+let formEl;
 
 document.addEventListener("DOMContentLoaded", async () => {
-  const res = await axios.get("/api/users");
-  console.log(res);
-  document.body.innerHTML = `
-    <img src="${nyancat}" />`;
+  formEl = document.createElement("div");
+  formEl.innerHTML = form.render();
+  document.body.appendChild(formEl);
+
+  resultEl = document.createElement("div");
+  resultEl.innerHTML = await result.render();
+  document.body.appendChild(resultEl);
 });
+
+if (module.hot) {
+  console.log("hot module 켜짐");
+
+  module.hot.accept("./result", async () => {
+    console.log("result modul 변경");
+    resultEl.innerHTML = await result.render();
+  });
+
+  module.hot.accept("./form", () => {
+    formEl.innerHTML = form.render();
+  });
+}
